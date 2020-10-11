6d63b072639de6f01578f430a728d54d106691ab
Vincensa Regina Indarto <vincensaregina@gmail.com>
2020-10-11 18:31:49 +0700

    revert changes 1
    revert

--- app/src/main/java/com/tubespbp/petshop/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/tubespbp/petshop/ui/profile/ProfileFragment.java
@@ -1,40 +1,39 @@
 package com.tubespbp.petshop.ui.profile;
 
-import android.os.AsyncTask;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
-import com.google.android.material.textview.MaterialTextView;
+import com.google.android.material.button.MaterialButton;
 import com.tubespbp.petshop.R;
-import com.tubespbp.petshop.ui.profile.database.DatabaseClientUser;
-import com.tubespbp.petshop.ui.profile.model.User;
-
-import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
-    private MaterialTextView email, name, username, phone, city, country;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        email = container.findViewById(R.id.et_email);
-        email = container.findViewById(R.id.et_email);
-
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         Button btnEdit = root.findViewById(R.id.btn_editProfile);
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,30 +43,5 @@ public class ProfileFragment extends Fragment {
             }
         });
         return root;
-    }
-
-    private void getUsers(){
-        class GetUsers extends AsyncTask<Void, Void, List<User>>{
-
-            @Override
-            protected List<User> doInBackground(Void... voids) {
-                List<User> userList = DatabaseClientUser
-                        .getInstance(getContext())
-                        .getDatabaseUser()
-                        .signUpDAO()
-                        .getAll();
-                return userList;
-            }
-
-            @Override
-            protected void onPostExecute(List<User> users) {
-                super.onPostExecute(users);
-                if (users.isEmpty()){
-
-                }
-            }
-        }
-        GetUsers get = new GetUsers();
-        get.execute();
     }
 }
