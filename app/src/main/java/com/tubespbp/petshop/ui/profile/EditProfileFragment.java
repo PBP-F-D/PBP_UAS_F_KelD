b4a49b34022fd9bc9e9c9b6913a3a9e39257155e
Vincensa Regina Indarto <vincensaregina@gmail.com>
2020-10-16 02:30:34 +0700

    edit profile cancel button

--- app/src/main/java/com/tubespbp/petshop/ui/profile/EditProfileFragment.java
+++ app/src/main/java/com/tubespbp/petshop/ui/profile/EditProfileFragment.java
@@ -111,7 +111,8 @@ public class EditProfileFragment extends Fragment {
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().finish();
+                Navigation.findNavController(view).navigate(
+                        R.id.action_editProfileFragment_to_navigation_notifications); //move to profile fragment
             }
         });
 
